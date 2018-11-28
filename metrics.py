6a631b0d5c62c5b855e19c8c386787044f08a6a0
Alexz29 <alexey@neronium.com>
2018-11-28 18:14:48 +0200

    fix

--- metrics.py
+++ metrics.py
@@ -35,7 +35,7 @@ if sys.argv[1]:
     except:
         raise Exception("Auth problem")
 
-    api.Tag.create(instance.node_name, tags=[conf['group']])
+    # api.Tag.create(instance.node_name, tags=[conf['group']])
 
     for monitor_delete_id in monitor_delete_ids:
         api.Monitor.delete(monitor_delete_id)
